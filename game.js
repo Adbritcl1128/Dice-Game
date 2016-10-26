271860799a143f2662ff0952b65dbd733e1a59a7
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2016-10-26 14:15:15 -0400

    dice randomized

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 (function diceGame() {
 
-}
+
     // Use document.querySelector('...') to find HTML elements
     // The argument into the function is a CSS selector!
     // Any HTML element has an `innerText` property that you can
@@ -9,21 +9,17 @@
     document.querySelector('button').addEventListener('click', function buttonClicked() {
 
       var dieone = Math.floor(Math.random()*6+1);
-      console.log(dieone);
 
       var dietwo = Math.floor(Math.random()*6+1);
-      console.log(dietwo);
 
+      document.querySelector('.dice aside:first-child').innerText = dieone;
 
-      var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
-      console.log(dieone);
+      document.querySelector('.dice aside:last-child').innerText = dietwo;
 
-      var dieNumTwo = document.querySelector('.dice aside:last-child').innerText;
-      console.log(dietwo);
 
       var diceTotal = dieone + dietwo;
 
-      status.innertext = 'You rolled ' + diceTotal + '.';
+      status.innertext = '' + diceTotal + '.';
 
     });
 
